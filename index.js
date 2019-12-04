54d0115ef82f1dfef6c855113620bba61ce54ae0
Anwesh43 <anweshthecool0@gmail.com>
2019-12-04 15:10:27 +0530

    adding Field and JsonField class

--- index.js
+++ index.js
@@ -1,25 +1,58 @@
-class KeyDiv {
+class Field {
+    constructor() {
+        this.div = document.createElement('div')
+    }
+
+    appendToParent(parent) {
+        parent.appendChild(this.div)
+    }
+}
+
+class KeyDiv extends Field {
 
     constructor(text) {
-        this.div = document.createElement('div')
+        super()
         this.div.className = 'textBlock'
         this.div.innerHTML = text
     }
 }
 
-class ValueField {
+class ValueField extends Field {
 
-    constructor() {
-        this.div = document.createElement('div')
+    constructor(text) {
+        super()
         this.text = document.createElement('input')
         this.btn = document.createElement('button')
+        this.addAttributes(text)
     }
 
-    addAttributes() {
+    addAttributes(text) {
         this.div.appendChild(this.text)
         this.div.appendChild(this.btn)
         this.div.className = 'valueBlock'
         this.btn.innerHTML = 'EDIT'
+        this.input.value = text
+    }
+}
 
+class JsonField extends Field {
+
+    constructor(key, value) {
+        super()
+        this.div.className = 'jsonField'
+        this.keyDiv = new KeyDiv(key)
+        this.parse(value)
+    }
+
+    parse(value) {
+        if (typeof(value) !== "object" || typeof(value) !== "function") {
+            this.valueDiv = new ValueField(value)
+            this.div.appendChild(this.valueDiv)
+        } else if (typeof(value) == "object") {
+              Object.keys(value).forEach((key) => {
+                  const valueField = new JsonField(key, value[key])
+                  valueField.appendToParent(this.div)
+              })
+        }
     }
 }
